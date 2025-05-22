9bda7c0e968645baa443d5917aec18ddc9c9c2d1
Anshika <124166933+anshikas14@users.noreply.github.com>
2025-05-22 20:53:31 +0530

    📖 deploy friendly build

---
+++ src/pages/NotesPage.jsx
@@ -0,0 +1,18 @@
+import NoteCard from '../components/NoteCard.jsx';
+import Controls from '../components/Controls.jsx';
+import { useContext } from 'react';
+import { NoteContext } from '../Context/NoteContext.jsx';
+
+const NotesPage = () => {
+   const {notes} = useContext(NoteContext);
+  return (
+    <div>
+      {notes.map(note => (
+      <NoteCard key={note.$id} note={note} />
+      ))}
+      <Controls />
+    </div>
+  )
+}
+
+export default NotesPage
